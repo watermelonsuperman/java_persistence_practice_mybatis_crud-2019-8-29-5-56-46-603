f536ef6085a6e67eb89681f77145fb228f8fff1e
watermelonsuperman <15538287230@163.com>
2019-08-29 16:47:59 +0800

    CRUD

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -2,10 +2,14 @@ package tws.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import tws.entity.Employee;
 import tws.repository.EmployeeMapper;
@@ -25,5 +29,20 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> getAll() {
         return ResponseEntity.ok(employeeMapper.selectAll());
     }
-
+    
+    @PostMapping
+    public void insert(@RequestBody Employee employee){
+    	employeeMapper.insert(employee);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+    	employeeMapper.delete(id);
+    }
+    
+    @PutMapping("/{id}")
+    public void update(@PathVariable int id,@RequestBody Employee employee){
+    	employeeMapper.update(id,employee);
+    	
+    }
 }
--- src/main/java/tws/repository/EmployeeMapper.java
+++ src/main/java/tws/repository/EmployeeMapper.java
@@ -1,6 +1,9 @@
 package tws.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import tws.entity.Employee;
 
 import java.util.List;
@@ -8,4 +11,7 @@ import java.util.List;
 @Mapper
 public interface EmployeeMapper {
     List<Employee> selectAll();
+    void insert (@Param(value = "employee")Employee employee);
+    void delete (@Param(value = "id")int id);
+    void update (@Param(value = "id")int id,@Param(value = "employee")Employee employee);
 }
